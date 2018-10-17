20ca50f56d34a57b2f9036124e644e9821345607
Aszek18 <pawelczykaga@gmail.com>
2018-10-17 18:12:33 +0200

    test

--- basics/przykład 15.10.py
+++ basics/przykład 15.10.py
@@ -1,10 +1,19 @@
 from random import randint
 
-graczX = randint(0,10)
-graczY = randint(0,10)
+graczX = randint(1,10)
+graczY = randint(1,10)
+
+skarbX = randint(1,10)
+skarbY = randint(1,10)
+
+
 
 while True:
     ruch = input("podaj jaki chcesz wykonać ruch: g, d, p, l:")
+
+    print(f"pozycja gracza to: {graczX, graczY}")
+
+    minKrokowPrzed =abs(skarbX-graczX) + abs(skarbY - graczY)
 
     if ruch == "g":
         graczY = graczY + 1
@@ -15,10 +24,23 @@ while True:
     elif ruch == "l":
         graczX = graczX - 1
 
+    minKrokowPo = abs(skarbX - graczX) + abs(skarbY - graczY)
+
     if (ruch == "koniec"):
         break
 
-print(f"pozycja gracza to: {graczX, graczY}")
+    if minKrokowPrzed < minKrokowPo:
+        print("oddalasz się")
+    else:
+        print("Zbliżasz się")
+
+    if graczX == skarbX and graczY == skarbY:
+        print("Znalazłeś skarb")
+        break
+
+    if graczX >10 or graczX <1 or graczY >10 or graczY <1:
+        print("Spadłes z planszy!")
+        break
 
 
 
